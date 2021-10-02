76db4837216f5ca2dc8b3e1fa5bfde8acf9c5566
Frank Elsinga <frank@elsinga.de>
2021-10-02 23:39:56 +0200

    Changed the desighn to be more in line of the settool and less "weird"

--- rallyetool/urls.py
+++ rallyetool/urls.py
@@ -1,13 +1,25 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
+from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import include, path
-
-import ratings.views as ratingsviews
+from django.views.generic import RedirectView
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("accounts/", include("accounts.urls")),
-    path("", ratingsviews.results, name="results"),
+    # Auth
+    path("login/", LoginView.as_view(template_name="login.html"), name="login"),
+    path("logout/", LogoutView.as_view(), name="logout"),
+    # localization
+    path("i18n/", include("django.conf.urls.i18n")),
+    # common: choose semester and settings
+    path("", include("common.urls")),
+    # Ratings
     path("ratings/", include("ratings.urls")),
+    # Challenges
     path("challenges/", include("challenges.urls")),
-    # path('signup/', ratingsviews.signup, name='signup')
+    # Admin
+    path("admin/", admin.site.urls),
+    # Index
+    path("", RedirectView.as_view(pattern_name="ratings:main-view"), name="main-view"),
 ]
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
