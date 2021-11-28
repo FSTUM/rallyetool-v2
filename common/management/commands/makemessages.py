571b025306c2b0df1577cf89b98b18cfeb7f440f
Frank Elsinga <frank@elsinga.de>
2021-11-28 23:21:58 +0100

    fixed makemessages not being in the pre-commit script

---
+++ common/management/commands/makemessages.py
@@ -0,0 +1,26 @@
+from django.core.management.commands import makemessages
+
+
+class Command(makemessages.Command):
+    def build_potfiles(self):
+        potfiles = super().build_potfiles()
+
+        for potfile in sorted(set(potfiles)):
+            self._remove_pot_creation_date(potfile)
+
+        return potfiles
+
+    @staticmethod
+    def _remove_pot_creation_date(path):
+        modified_lines = []
+
+        with open(path, "rb") as file:
+            for line in file:
+                if line.startswith(b'"Report-Msgid-Bugs-To:'):
+                    modified_lines.append(b'"Report-Msgid-Bugs-To: elsinga <at> fs.tum.de\\n"')
+                elif not line.startswith(b'"POT-Creation-Date: '):
+                    modified_lines.append(line)
+
+        with open(path, "wb") as file:
+            for line in modified_lines:
+                file.write(line)
