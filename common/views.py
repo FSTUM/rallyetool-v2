d13e7581a1964b975b961c295b5bee7f39e4b40b
Frank Elsinga <frank@elsinga.de>
2021-12-11 23:56:13 +0100

    migrated to django 4.0

--- common/views.py
+++ common/views.py
@@ -6,7 +6,7 @@ from django.contrib.auth.models import User  # pylint: disable=imported-auth-use
 from django.core.handlers.wsgi import WSGIRequest
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import get_object_or_404, redirect, render
-from django.utils.http import is_safe_url
+from django.utils.http import url_has_allowed_host_and_scheme
 from django.utils.translation import gettext as _
 
 from common.models import get_semester, Semester, Settings
@@ -26,9 +26,9 @@ superuser_required: Callable = user_passes_test(lambda u: u.is_superuser)
 @rallye_login_required
 def set_semester(request: AuthWSGIRequest) -> HttpResponse:
     redirect_url: Optional[str] = request.POST.get("next") or request.GET.get("next")
-    if not is_safe_url(url=redirect_url, allowed_hosts=request.get_host()):
+    if not url_has_allowed_host_and_scheme(url=redirect_url, allowed_hosts=request.get_host()):
         redirect_url = request.META.get("HTTP_REFERER")
-        if not is_safe_url(url=redirect_url, allowed_hosts=request.get_host()):
+        if not url_has_allowed_host_and_scheme(url=redirect_url, allowed_hosts=request.get_host()):
             redirect_url = "/"  # should not happen :)
     if request.method == "POST":
         semester_pk = int(request.POST.get("semester") or -1)  # semester is always present
