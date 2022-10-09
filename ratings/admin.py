13efc0190f80f771be45df8ce83c3a2598f93d5e
Frank Elsinga <frank@elsinga.de>
2022-10-09 23:43:10 +0200

    added all the important models to the admin site

--- ratings/admin.py
+++ ratings/admin.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
 
-from .models import Group, Rating, Station
+from .models import Group, Rating, Station, RatingScheme1, RatingScheme2, RatingScheme3, RatingScheme3Group
 
 admin.site.register(Group)
 admin.site.register(Rating)
 admin.site.register(Station)
+admin.site.register(RatingScheme1)
+admin.site.register(RatingScheme2)
+admin.site.register(RatingScheme3)
+admin.site.register(RatingScheme3Group)
